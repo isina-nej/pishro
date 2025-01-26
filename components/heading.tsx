5d03930e4eebf1993bbe687c79019faff61ace5f
amir-9 <aghaamirjan13@gmail.com>
2025-01-27 02:17:16 +0330

    added link

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
+
 interface HeadingProps {
   children: string;
+  link?: string | React.JSX.Element;
+  href?: string;
 }
 
-const Heading = ({ children }: HeadingProps) => {
+const Heading = ({ children, link, href }: HeadingProps) => {
   return (
     <div>
-      <h2 className="font-bold text-[22px] mb-4">{children}</h2>
+      <div className="flex justify-between items-center">
+        <h2 className="font-bold text-[22px] mb-4">{children}</h2>
+        {link && <Link href={href || "#"}>{link}</Link>}
+      </div>
       <div className="h-[2px] w-full bg-[#e1e1e1] relative rounded-sm">
         <div className="absolute bottom-0 right-0 h-[3px] w-1/4 bg-myPrimary rounded-sm"></div>
       </div>
