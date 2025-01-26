975181e9cb71067dfbfa4cbf4243b148f29b42cb
amir-9 <aghaamirjan13@gmail.com>
2025-01-26 21:31:55 +0330

    added iranSansX font

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 import { Input } from "@/components/ui/input";
-import { SearchNormalIcon, FilterIcon } from "@/public/svgr-icons";
+import {
+  SearchNormalIcon,
+  FilterIcon,
+  UserIcon,
+  BuyIcon,
+} from "@/public/svgr-icons";
 
 const Navbar = () => {
   return (
@@ -7,10 +14,10 @@ const Navbar = () => {
       <div className="px-3 py-5 h-20 flex justify-between items-center">
         {/* بخش سمت راست */}
         <div className="w-full max-w-[650px] flex items-center gap-6">
-          <div className="h-10 w-[100px] text-green-600 text-[32px] font-bold ltr">
-            logo
+          <div className="h-10 w-[100px] flex items-center">
+            <Image src={"/icons/Logo.png"} alt="logo" width={90} height={32} />
           </div>
-          <div className="relative h-8 w-full max-w-[400px]">
+          <div className="relative h-8 w-full max-w-[526px]">
             {/* آیکون جستجو */}
             <button className="absolute top-1/2 transform -translate-y-1/2 h-8 px-4">
               <SearchNormalIcon width={12} height={12} />
@@ -22,13 +29,24 @@ const Navbar = () => {
               className="pl-12 pr-12 w-full h-8"
             />
             {/* آیکون فیلتر */}
-            <button className="absolute left-0 top-1/2 transform -translate-y-1/2 h-8 size-7 bg-[#EDF4F8]">
+            <button className="absolute left-0 top-1/2 transform -translate-y-1/2 h-full w-7 bg-[#EDF4F8] border-transparent flex justify-center items-center rounded-l-sm">
               <FilterIcon width={20} height={20} />
             </button>
           </div>
         </div>
         {/* بخش سمت چپ */}
-        <div>left</div>
+        <div className="flex items-center gap-7">
+          <button className="flex items-center gap-1">
+            <UserIcon width={18} height={18} />
+            <span className="font-medium text-xs"> ورود یا ثبت نام</span>
+          </button>
+          <button className="flex items-center gap-1">
+            <BuyIcon width={18} height={18} />
+            <span className="text-white bg-[#D52A16] size-4 rounded-[2px] text-xs font-bold text-center">
+              6
+            </span>
+          </button>
+        </div>
       </div>
       <div>down</div>
     </nav>
