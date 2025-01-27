b6a236105ecc7c40a718ad7da92ac7509e059a84
amir-9 <aghaamirjan13@gmail.com>
2025-01-27 13:45:05 +0330

    .

--- components/utils/heading.tsx
+++ components/utils/heading.tsx
@@ -11,7 +11,11 @@ const Heading = ({ children, link, href }: HeadingProps) => {
     <div>
       <div className="flex justify-between items-center">
         <h2 className="font-bold text-[22px] mb-4">{children}</h2>
-        {link && <Link href={href || "#"}>{link}</Link>}
+        {link && (
+          <Link href={href || "#"} className="text-sm text-[#666666]">
+            {link}
+          </Link>
+        )}
       </div>
       <div className="h-[2px] w-full bg-[#e1e1e1] relative rounded-sm">
         <div className="absolute bottom-0 right-0 h-[3px] w-1/4 bg-myPrimary rounded-sm"></div>
