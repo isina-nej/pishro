343d40f3d176060e39b0ef1b48d18d630fcd0b9e
amir-9 <aghaamirjan13@gmail.com>
2025-01-29 17:21:37 +0330

    .

--- components/utils/Banner.tsx
+++ components/utils/Banner.tsx
@@ -2,7 +2,9 @@ const Banner = () => {
   return (
     <div className="flex w-full h-[160px] bg-[#f5f5f5] mt-10 container">
       <div className="flex-1 flex items-center">
-        <h2 className="font-bold text-2xl">لورم ایپسوم متن ساختگی</h2>
+        <h2 className="font-bold text-2xl">
+          بدون هیچ ضامن و اتلاف وقتی؛ اعتبار بگیرید
+        </h2>
       </div>
       <div
         className="flex-1 bg-no-repeat bg-cover bg-center"
