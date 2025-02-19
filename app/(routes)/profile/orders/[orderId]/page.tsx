a3f74aa4382568851ed91f16d8045793141348a5
amir-9 <aghaamirjan13@gmail.com>
2025-02-19 19:10:34 +0330

    3rd

--- app/(routes)/profile/orders/[orderId]/page.tsx
+++ app/(routes)/profile/orders/[orderId]/page.tsx
@@ -2,19 +2,14 @@ import { profileOrdersData } from "@/public/data";
 import OrderDetail from "@/components/profile/orderDetail";
 
 interface OrderPageProps {
-  params:
-    | {
-        orderId: string; // اگر پوشه به صورت [orderId] تعریف شده است، params.orderId یک رشته است.
-      }
-    | Promise<{
-        orderId: string; // اگر پوشه به صورت [orderId] تعریف شده است، params.orderId یک رشته است.
-      }>;
+  params: {
+    orderId: string; // اگر پوشه به صورت [orderId] تعریف شده است، params.orderId یک رشته است.
+  };
 }
 
-export default async function OrderPage({ params }: OrderPageProps) {
+export default function OrderPage({ params }: OrderPageProps) {
   // await کردن params برای رفع خطا
-  const awaitedParams = await params;
-  const { orderId } = awaitedParams;
+  const { orderId } = params;
 
   // جستجو در داده‌های محلی بر اساس payId
   const order = profileOrdersData.find(
