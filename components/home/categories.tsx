bb1c2bab28b191cf1c30c78c851e792772ec297a
amir-9 <aghaamirjan13@gmail.com>
2025-02-27 00:00:03 +0330

    adding new type of categories for test

--- components/home/categories.tsx
+++ components/home/categories.tsx
@@ -1,26 +1,57 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { categoriesData } from "@/public/data";
-
+export const categoriesData = [
+  {
+    src: "/images/home/crypto.jpg",
+    label: "کریپتوکارنسی",
+    link: "/cryptocurrency",
+  },
+  {
+    src: "/images/home/stock.png",
+    label: "بورس",
+    link: "/stock-market",
+  },
+  {
+    src: "/images/home/metaverse.png",
+    label: "متاورس",
+    link: "/metaverse",
+  },
+  {
+    src: "/images/home/nft.png",
+    label: "NFT",
+    link: "/nft",
+  },
+  {
+    src: "/images/home/airdrop.png",
+    label: "ایردراپ",
+    link: "/airdrop",
+  },
+];
 const Categories = () => {
   return (
-    <div className="mt-10 container">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-[72px]">
-        {categoriesData.map((item, idx) => (
-          <div key={idx} className="text-center">
-            <Link href={item.link} className="block">
-              <Image
-                src={item.src}
-                alt={item.label}
-                width={154}
-                height={154}
-                className="w-full object-cover mb-8 rounded-full overflow-hidden"
-              />
-              <span className="text-lg font-semibold">{item.label}</span>
-            </Link>
-          </div>
-        ))}
+    <div className="mt-20 container">
+      <div className="max-w-3xl mx-auto">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          {categoriesData.map((item, idx) => (
+            <div key={idx} className="text-center">
+              <Link
+                href={item.link}
+                className="flex flex-col gap-6 justify-center items-center"
+              >
+                <div className="relative size-20">
+                  <Image
+                    src={item.src}
+                    alt={item.label}
+                    fill
+                    className="w-full object-cover mb-8 rounded-full overflow-hidden"
+                  />
+                </div>
+                <span className="text-lg font-medium">{item.label}</span>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/home/homeContent.tsx
+++ components/home/homeContent.tsx
@@ -6,11 +6,15 @@ import BusinessConsulting from "@/components/home/businessConsulting";
 import Courses from "@/components/home/courses";
 import Banner from "@/components/utils/Banner";
 import Blog from "@/components/utils/blog";
+import Categories2 from "./categories2";
+import Categories3 from "./categories3";
 
 const HomePageContent = () => {
   return (
     <div>
       <Landing />
+      <Categories3 />
+      <Categories2 />
       <Categories />
       <WhyUs />
       <HomeSlider />
