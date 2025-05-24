baa9eaa50378febc69b89c3451a446379f915cdd
amir-9 <aghaamirjan13@gmail.com>
2025-05-24 17:03:53 +0330

    updated crypto page

--- components/cryptocurrency/sectionOne.tsx
+++ components/cryptocurrency/sectionOne.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import LottieRemote from "@/components/utils/LottieAnimation";
 
 const SectionOne = () => {
   return (
-    <div className="container my-40 relative flex flex-col items-center ">
-      {/* section one */}
+    <div className="container my-40 relative flex flex-col items-center">
+      {/* بخش اول */}
       <div className="flex justify-between items-center gap-20">
-        <div className="flex-[2]">
+        <div className="flex-1">
           <p className="text-lg text-[#707177] leading-9">
             رمزارزها انقلاب دیجیتال در دستان شما! در دنیای امروز، رمزارزها تنها
             یک دارایی دیجیتال نیستند، بلکه دری به سوی آینده‌ای نوین در دنیای
@@ -17,36 +17,33 @@ const SectionOne = () => {
             سرمایه‌گذاری کنید! آینده مالی شما از اینجا شروع می‌شود.
           </p>
         </div>
-        <div className="flex-[3] h-[300px]">
-          <div className="size-full relative rounded-lg overflow-hidden">
-            <Image
-              src={"/images/charisma-img.svg"}
-              alt="crypto"
-              fill
-              className="object-contain"
+        <div className="flex-1 h-[300px]">
+          <div className="w-full h-full rounded-lg overflow-hidden">
+            <LottieRemote
+              path="/animations/cryptocurrency-analytic.json"
+              className="w-full h-full"
             />
           </div>
         </div>
       </div>
-      {/* section two */}
+
+      {/* بخش دوم */}
       <div className="flex justify-between items-center gap-20 mt-32">
-        <div className="flex-[3] h-[300px]">
-          <div className="size-full relative rounded-lg overflow-hidden">
-            <Image
-              src={"/images/charisma-img.svg"}
-              alt="crypto"
-              fill
-              className="object-contain"
+        <div className="flex-1 h-[300px]">
+          <div className="w-full h-full rounded-lg overflow-hidden">
+            <LottieRemote
+              path="/animations/boy-investing-in-bitcoin.json"
+              className="w-full h-full"
             />
           </div>
         </div>
-        <div className="flex-[2]">
+        <div className="flex-1">
           <p className="text-lg text-[#707177] leading-9">
-            راهی برای بدست اوردن توکن های پروژه کریپتویی قبل از عرضه به بازار به
-            صورت رایگان است   شاید باورتان نشود ولی بیشتر ایردراپ ها را میتوان
-            به صورت رایگان هم دریافت کرد و تنها کافیست گام هایی را که برای
-            دریافت آن تعریف شده طی نمود . حضور در بازار NFT ها تقریبا بدون ریسک
-            است و تنها کافیست برای این کار اندکی زمان بگذارید
+            راهی برای بدست آوردن توکن‌های پروژه‌های کریپتویی قبل از عرضه به
+            بازار به‌صورت رایگان است. شاید باورتان نشود، ولی بیشتر ایردراپ‌ها را
+            می‌توان بدون پرداخت هزینه دریافت کرد؛ تنها کافی است مراحل مشخص‌شده
+            را طی کنید. حضور در بازار NFTها تقریباً بدون ریسک است و تنها کافی‌ست
+            اندکی زمان بگذارید.
           </p>
         </div>
       </div>
