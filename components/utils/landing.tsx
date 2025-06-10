acf38f70f600e2b91477dd8a276d3fb8ad5bd7cb
amir-9 <aghaamirjan13@gmail.com>
2025-06-10 18:18:37 +0330

    changed dark overly

--- components/utils/landing.tsx
+++ components/utils/landing.tsx
@@ -6,9 +6,10 @@ import { cn } from "@/lib/utils";
 
 interface LandingProps {
   size?: "small" | "normal";
+  darker?: boolean;
 }
 
-const Landing = ({ size = "normal" }: LandingProps) => {
+const Landing = ({ size = "normal", darker }: LandingProps) => {
   const pathname = usePathname();
 
   // داده پیش‌فرض در صورتی که مسیر مطابقت نداشته باشد
@@ -25,7 +26,9 @@ const Landing = ({ size = "normal" }: LandingProps) => {
       style={{ backgroundImage: `url('${data.backgroundImage}')` }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/30"></div>
+      <div
+        className={cn("absolute inset-0 bg-black/30", darker ? "" : "hidden")}
+      ></div>
 
       <div className="relative mt-[140px] w-full mx-[90px]">
         <h1 className="text-2xl md:text-4xl font-bold mb-6">{data.title}</h1>
