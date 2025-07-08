60884117d8af10a9cba61ea0b3cdc264dda28b06
amir-9 <aghaamirjan13@gmail.com>
2025-07-08 23:17:19 +0330

    added video and video player

--- components/home/homeContent.tsx
+++ components/home/homeContent.tsx
@@ -1,11 +1,12 @@
 import Categories from "@/components/home/categories";
 import WhyUs from "@/components/home/whyUs";
-import HomeSlider from "@/components/utils/homeSlider";
+// import HomeSlider from "@/components/utils/homeSlider";
 import BusinessConsulting from "@/components/home/businessConsulting";
 import Courses from "@/components/home/courses";
 import Banner from "@/components/utils/Banner";
 import Blog from "@/components/utils/blog";
 import Landing from "../utils/landing";
+import TransparentVideoPlayer from "../utils/TransparentVideoPlayer";
 
 const HomePageContent = () => {
   return (
@@ -13,7 +14,8 @@ const HomePageContent = () => {
       <Landing size="normal" />
       <Categories />
       <WhyUs />
-      <HomeSlider />
+      {/* <HomeSlider /> */}
+      <TransparentVideoPlayer src="/videos/landing-vid.webm" />
       <BusinessConsulting />
       <Banner />
       <Courses />
