d1b28927be27d7bc44ba95721f61bd59a240c82f
amir-9 <aghaamirjan13@gmail.com>
2025-07-08 22:20:13 +0330

    edited logo

--- components/utils/Logo.tsx
+++ components/utils/Logo.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import { cn } from "@/lib/utils";
 
 const Logo = () => {
   return (
-    <div className="h-10 w-[100px] flex items-center">
-      <Image src={"/icons/Logo.png"} alt="logo" width={90} height={32} />
+    <div
+      className={cn(
+        "h-8 w-[90px] flex items-center justify-center gap-1",
+        "p-1 -mb-1 ltr",
+        "text-gray-800 text-sm font-semibold tracking-tight",
+        "hover:text-gray-900",
+        "transition-all duration-200 ease-in-out"
+      )}
+    >
+      <div className="uppercase font-bold">
+        <span className="text-red-600">p</span>
+        ishro
+      </div>
+      <div className="text-sm font-medium text-gray-600">
+        <span className="text-red-600">Gp</span>
+      </div>
     </div>
   );
 };
