61e49b3e2c8f1dac2a84c25c8e2430dc13c13b92
amir-9 <aghaamirjan13@gmail.com>
2025-07-30 23:19:56 +0330

    edited courses

--- components/home/courses.tsx
+++ components/home/courses.tsx
@@ -1,17 +1,11 @@
-import { ArrowCircleLeftIcon } from "@/public/svgr-icons";
 import { coursesData } from "@/public/data";
 import Heading from "@/components/utils/heading";
 import CourseCard from "@/components/home/courseCard";
 
 const Courses = () => {
   return (
-    <div className="container mt-20">
-      <Heading
-        link={<ArrowCircleLeftIcon width={24} height={24} />}
-        href="/courses"
-      >
-        دوره های آموزشی
-      </Heading>
+    <div className="container-xl mt-20">
+      <Heading>دوره های منتخب</Heading>
       <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {coursesData.map((data, idx) => (
           <CourseCard
