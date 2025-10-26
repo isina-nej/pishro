5cca5364d22b1a340f6eb8c8bcca0db5600739b6
amir-9 <aghaamirjan13@gmail.com>
2025-10-26 15:17:32 +0330

    deleted snap scroll

--- components/home/homeContent.tsx
+++ components/home/homeContent.tsx
@@ -7,31 +7,17 @@ import BikeSection from "./bikeSection";
 import Courses from "./courses";
 import CommentsSlider from "@/components/utils/CommentsSlider";
 import NewsClub from "./newsClub";
-import SnapSingleSection from "@/components/utils/SnapSingleSection";
 
 const HomePageContent = () => {
   return (
     <div className="w-full">
       <LandingOverlay />
-
-      <SnapSingleSection id="mobile-section">
-        <MobileScrollSection />
-      </SnapSingleSection>
-      <SnapSingleSection id="calculator-section">
-        <CalculatorSection />
-      </SnapSingleSection>
-      <SnapSingleSection id="bike-section">
-        <BikeSection />
-      </SnapSingleSection>
-      <SnapSingleSection id="courses-section">
-        <Courses />
-      </SnapSingleSection>
-      <SnapSingleSection id="comments-section">
-        <CommentsSlider />
-      </SnapSingleSection>
-      <SnapSingleSection id="newsClub-section">
-        <NewsClub />
-      </SnapSingleSection>
+      <MobileScrollSection />
+      <CalculatorSection />
+      <BikeSection />
+      <Courses />
+      <CommentsSlider />
+      <NewsClub />
     </div>
   );
 };
