1f11504c32359ce223027871e5e5aab6882c6ef2
amir-9 <aghaamirjan13@gmail.com>
2025-11-06 17:31:12 +0330

    deleted unused comps

--- components/investment-consulting/pageContent.tsx
+++ components/investment-consulting/pageContent.tsx
@@ -4,18 +4,6 @@ const InvestmentPageContent = () => {
   return (
     <div>
       <InvestmentLanding />
-      {/* <AlibabaSlider
-        topImages={alibabaData.topImages}
-        middleImages={alibabaData.middleImages}
-        bottomImages={alibabaData.bottomImages}
-      />
-      <div className="h-32"></div>
-      <VideoSection videoUrl="/videos/crypto.webm" label="سرمایه گذاری پیشرو" />
-      <div className="mb-24 mt-8 flex justify-center">
-        <QAModal />
-      </div>
-      <Courses />
-      <Blog /> */}
     </div>
   );
 };
