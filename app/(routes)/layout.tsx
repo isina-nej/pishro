5eba0c9155990bdde07e9503d7ba8d9fcca393cf
amir-9 <aghaamirjan13@gmail.com>
2025-11-13 19:43:12 +0330

    added floating checkout card

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer";
 import ChatWidget from "@/components/utils/ChatWidget";
 import ScrollToTopButton from "@/components/utils/ScrollToTopButton";
+import FloatingCartButton from "@/components/utils/FloatingCartButton";
 import { SessionProvider } from "next-auth/react";
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
       {children}
       <Footer />
       <ScrollToTopButton />
+      <FloatingCartButton />
       <ChatWidget />
     </SessionProvider>
   );
